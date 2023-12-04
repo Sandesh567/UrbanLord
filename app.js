27dba3453ef6dd78abc88016a82f10deef8a0945
Sandesh Raj Bhatta <106328473+Sandesh567@users.noreply.github.com>
2023-12-04 19:37:33 +0545

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const tlLeave = gsap.timeline({
 const tlEnter = gsap.timeline({
     defaults:{duration:0.75 , ease : "Power2.easeOut"},
 });
-//Make the functions for the leavr and enter animations
+//Make the functions for the leave and enter animations
 
 const leaveAnimation = (current,done) => {
     const product = current.querySelector(".image-container");
@@ -13,14 +13,60 @@ const leaveAnimation = (current,done) => {
     const circles = current.querySelector(".circle");
     const arrow = current.querySelector(".showcase-arrow");
     return(
-        tlLeave.fromTo(arrow,{opacity:1 , y:0} , {opacity: 0 , y: 500}),
+        tlLeave.fromTo(arrow,{opacity:1 , y:0} , 
+            {opacity: 0 , y: 50}),
         tlLeave.fromTo(
             product,
             {y:0 , opacity :1},
-            {y:100,opacity:0,onComplete:done},"<"
+            {y:100,opacity:0},"<"
         ),
-        tlLeave.frommTo(text,{y:0 , opacity:1}, {opacity:0 , y:100},"<")
-    );
+        tlLeave.fromTo(text,{y:0 , opacity:1}, 
+            {opacity:0 , y:100,onComplete:done},"<")
+    ),
+    tl.Leave.fromTo(
+        circles,
+        {y:0 , opacity : 1},
+        {
+            y:-200,
+            opacity:0,
+            stagger:0.15,
+            ease:"back.out(1.7)",
+            duration : 1,
+        
+        },
+        "<"
+    )
+    
+};
+
+const enterAnimation = (current,done , gradient) => {
+    const product = current.querySelector(".image-container");
+    const text = current.querySelector(".showcase-text");
+    const circles = current.querySelector(".circle");
+    const arrow = current.querySelector(".showcase-arrow");
+    return(
+        tlEnter.fromTo(arrow,{opacity:0 , y:50} , 
+            {opacity: 1 , y: 0}
+        ),
+    tlEnter.to('body' , {background : gradient},"<"),
+    
+        tlEnter.fromTo(product, {y:-100 , opacity :0}, {y:0,opacity:1},"<"),
+        tlEnter.fromTo(text,{y:100 , opacity:0}, {opacity:1 , y:0,onComplete:done},"<")
+    ),
+    tlEnter.fromTo(
+        circles,
+        {y:-200 , opacity : 0},
+        {
+            y:0,
+            opacity:1,
+            stagger:0.15,
+            ease:"back.out(1.7)",
+            duration : 1,
+        
+        },
+        "<"
+    )
+
 };
 
 
@@ -31,7 +77,13 @@ barba.init({
         //showcase transitions
         {
             name : "default",
-
+            once(data){
+                const done  = this.async();
+                let next = data.next.container;
+                let gradient  = getGradient(data.next.namespace);
+                gsap.set('body',{backgorund : gradient});
+                enterAnimation(next,done,gradient);
+            },
             leave(data){
                 const done = this.async();
                 let current = data.current.container;
@@ -40,12 +92,29 @@ barba.init({
             enter(data){
                 const done = this.async();
                 let next = data.next.container;
-                gsap.fromTo(next,{opacity: 0} ,
-                 {opacity: 1, duration:1,onComplete : done});
+                let gradient = getGradient(data.next.namespace);
+                enterAnimation(next,done,gradient);
             },
         },
     ],
 });
+
+
+//changing gradient on showcase
+
+
+function getGradient(name){
+    switch(name){
+        case"handbag":
+            return "linear-gradient(260deg , #b75d62, #754d4f)";
+        case "boot":
+            return "linear-gradient(260deg , #5d8cb7, #4c4f70)";
+        case "hat":
+            return "linear-gradient(260deg , #b27a5c, #7f5450)";
+
+
+    }
+}
 
 
 
